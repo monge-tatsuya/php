50a13fa09dde90cc6903ec79e1c10b526b253c84
tatsuya <tatsuya3170@gmail.com>
2025-10-23 12:51:25 +0900

    リモートリポジトリの変更

---
+++ src/php01/index04.php
@@ -0,0 +1,21 @@
+<?php
+$a = 15;
+$b = 3;
+$c = 10;
+
+$ancer1 = $a + $b;
+$ancer2 = $a - $b;
+$ancer3=$a*$b;
+$ancer4=$a/$b;
+$ancer5=$a%$c;
+
+echo $ancer1;
+echo "<br />";
+echo $ancer2;
+echo "<br />";
+echo $ancer3;
+echo "<br />";
+echo $ancer4;
+echo "<br />";
+echo $ancer5;
+echo "<br />";
